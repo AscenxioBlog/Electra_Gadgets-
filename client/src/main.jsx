6a503155bcc747ddf84e04fd94ee41bd15b4dad2
Ascenxio Blog <hassanyusuuf122@gmail.com>
2024-09-06 17:08:01 +0100

    ascenxio

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Container from './Container'
-import './index.css'
+import React from 'react'; // Make sure to import React
+import { createRoot } from 'react-dom/client'; // Correct import for createRoot
+import Container from './Container';
+import './index.css';
 
-
-const root = ReactDom.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root')); // Use createRoot directly
+root.render(
   <React.StrictMode>
     <Container />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
