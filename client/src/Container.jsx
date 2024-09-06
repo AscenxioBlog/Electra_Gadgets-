c44a52e0523fda0032b6e2825cf263131a505673
Ascenxio Blog <hassanyusuuf122@gmail.com>
2024-09-06 13:01:13 +0100

    last

--- client/src/Container.jsx
+++ client/src/Container.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 function Container() {
   return (
     <div>
+      <BrowserRouter>
+      
+      
+      
+      </BrowserRouter>
         
     </div>
   )
