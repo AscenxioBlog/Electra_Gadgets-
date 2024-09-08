0510da646e5368d65140a91c5b902465df3846b8
Ascenxio Blog <hassanyusuuf122@gmail.com>
2024-09-08 22:51:37 +0100

    last

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import Container from './Container'
 import './index.css'
+import './App.css'
+
 
 
 const root = createRoot(document.getElementById('root')).render(
