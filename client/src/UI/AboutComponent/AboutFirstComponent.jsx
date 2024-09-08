c8ca1b25ea1d0ae8898d7a7aba322945cc9933c7
chidera-collins <chideracollins24@gmail.comm>
2024-09-08 23:01:52 +0100

    dera

--- client/src/UI/AboutComponent/AboutFirstComponent.jsx
+++ client/src/UI/AboutComponent/AboutFirstComponent.jsx
@@ -3,7 +3,10 @@ import CustomButton from '../../ReusableComponent/BtnComponent/CustomBtnComponen
 
 function AboutFirstComponent() {
   return (
-    <div>
+    <div className='bg-[yellow] h-[100vh] w-full'>
+      <h1>
+        hello world
+      </h1>
 
     </div>
   )
