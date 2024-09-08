cf07012726f902896fc5aae28f027f25a2757677
chidera-collins <chideracollins24@gmail.comm>
2024-09-08 22:44:28 +0100

    dera checked

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import  React from 'react'
 import { createRoot } from 'react-dom/client'
 import Container from './Container'
 import './index.css'
 
 
-const root = ReactDom.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Container />
   </React.StrictMode>,
