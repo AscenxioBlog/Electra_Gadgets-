bb113adfdfdd76933c1bddc374d5f9d91bc6cbe8
Ascenxio Blog <hassanyusuuf122@gmail.com>
2024-09-08 22:59:57 +0100

    last

--- client/src/UI/HomeComponent/IndexComponent.jsx
+++ client/src/UI/HomeComponent/IndexComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function IndexComponent() {
   return (
-    <div className=''>IndexComponent</div>
+    <div className='h-[100vh] w-full bg-slate-500'>IndexComponent</div>
   )
 }
 
