88714334d78f431b89759786a2969c81d917201b
chidera-collins <chideracollins24@gmail.comm>
2024-09-09 01:59:43 +0100

    Dera Aboutcomponent2

--- client/src/UI/AboutComponent/AboutFirstComponent.jsx
+++ client/src/UI/AboutComponent/AboutFirstComponent.jsx
@@ -4,9 +4,9 @@ import CustomButton from '../../ReusableComponent/BtnComponent/CustomBtnComponen
 function AboutFirstComponent() {
   return (
         // my header component 
-    <div className='bg-[yellow] h-[200px] w-[100vw] mt-[70px] aboutH flex items-center justify-center flex-col font-sans md:h-[320px]  md:mt-10 lg:h-[700px]'>
-      <div className='text-white flex items-center justify-center flex-col p-[20px] '>
-            <h1 className='font-bold  mt-[80px] text-[1.5rem] md:text-[2rem] lg:text-[3rem]'> WE ARE ELECTRA GADGETS</h1>
+    <div className='bg-[yellow] h-[200px] w-full mt-[70px] aboutH flex items-center justify-center flex-col font-sans md:h-[320px]  md:mt-10 lg:h-[700px] box-border'>
+      <div className='text-white flex items-center justify-center flex-col p-[20px]  box-border'>
+            <h1 className='font-bold  mt-[10px] text-[1.5rem] md:text-[2rem] lg:text-[3rem]'> WE ARE ELECTRA GADGETS</h1>
 
             <div className=' before:contents-[""] before:w-[60px] before:h-[30px] before:bg-[red]'></div>
 
@@ -18,10 +18,10 @@ function AboutFirstComponent() {
      
      
 
-      <CustomButton
+      {/* <CustomButton
         height= '100px'
       
-      />
+      /> */}
 
     </div>
   )
--- client/src/UI/AboutComponent/AboutSecondComponent.jsx
+++ client/src/UI/AboutComponent/AboutSecondComponent.jsx
@@ -2,7 +2,39 @@ import React from 'react'
 
 function AboutSecondComponent() {
   return (
-    <div>AboutSecondComponent</div>
+    <div className='h-[200px] w-full mt-[30px] lg:w-[80%] lg:translate-x-[147px] text-start'>
+
+      <div className=' lg:flex lg:items-center lg:gap-[20px] md:flex md:items-center md:gap-[20px] p-[10px] box-border'>
+
+                       
+        <div>
+            <h6>ABOUT US</h6>
+            <h1 className='font-bold text-[2.3rem] '>
+                WE LOVE MUSIC
+            </h1>
+
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam exercitationem ipsum illum totam enim non 
+            </p>
+
+          </div>
+          <div className='lg:flex lg:items-center lg:gap-[10px] lg:flex-col md:flex md:items-center md:gap-[20px] md:flex-col'>
+            <div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorem ab labore animi provident deserunt aliquam commodi! Amet accusantium labore reiciendis fuga obcaecati enim, pariatur deserunt iusto minus consequuntur deleniti necessitatibus! Illo dolore deserunt nisi, porro iste assumenda maxime rem!
+                </p>
+            </div>
+            <div> 
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ipsa iste deserunt dolore veniam? Distinctio aperiam tempora dolore? Dicta, eaque? Porro odio vel cumque sed? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut, perspiciatis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, tempore.
+                  </p>
+            </div>
+         
+          </div>
+      </div>
+   
+    </div>
+    
   )
 }
 
