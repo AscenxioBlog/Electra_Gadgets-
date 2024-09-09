3a2dbf8db412aa9453e0cb8824f601edff85117e
Ascenxio Blog <hassanyusuuf122@gmail.com>
2024-09-09 20:08:56 +0100

    updated by ascenxio

--- client/src/UI/ContactComponent/ContactComponent.jsx
+++ client/src/UI/ContactComponent/ContactComponent.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ContactComponent1 from './ContactComponent1'
+import ContactComponent2 from './ContactComponent2'
 
 function ContactComponent() {
   return (
 
 
-    <div className=' '>
+    <div className=' mt-[70px]'>
       <ContactComponent1/>
+      <ContactComponent2/>
     </div>
 
   )
--- client/src/UI/ContactComponent/ContactComponent1.jsx
+++ client/src/UI/ContactComponent/ContactComponent1.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function ContactComponent1() {
   return (
-    <div className=' h-[100vh] w-full bg-MainBg '>
+    <div className=' first h-[130vh] w-full flex justify-center items-center '>
+      <div className=" h-[300px] w-[80%] bg-[] flex flex-col items-center justify-center">
+        <div className=" text-[25px] md:text-[30px] lg:text-[40px] font-extrabold text-white">
+          <h1>CONTACT US</h1>
+        </div>
+        <div className=" flex gap-3">
+            <section className=' h-[3px] w-[20px] bg-MainBg' style={{borderTopLeftRadius:'10px', borderBottomLeftRadius: '10px'}}></section>
+            <section className=' h-[3px] w-[70px] bg-MainBg'></section>
+            <section className=' h-[3px] w-[20px] bg-MainBg' style={{borderBottomRightRadius: ' 10px', borderTopRightRadius: '10px'}}></section>
+        </div>
+        <div className=" w-full flex flex-col items-center space-y-4 mt-4 text-white text-[18px]">
+          <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debiti magni facere.</span>
+          <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span> 
+          <span>Lorem ipsum dolor sit amet.</span>
+        </div>
+      </div>
+        
         
       
     </div>
