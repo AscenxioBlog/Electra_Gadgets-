932c2f3c80ccd10f92d3c17e6fdb68b3a23ebc8a
Ascenxio Blog <hassanyusuuf122@gmail.com>
2024-09-10 11:44:26 +0100

    Updated by ascenxio

---
+++ client/src/UI/ContactComponent/ContactComponent3.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ContactComponent3() {
+  return (
+    <div className=' h-[100vh] w-full bg-[] flex justify-center items-center'>
+      <div className=" h-[90vh] w-full md:w-[95%] lg:w-[80%] bg-btncolo flex justify-center items-center">
+        <h1 className='text-[40px] font-bold'>MAP</h1>
+      </div>
+    </div>
+  )
+}
+
+export default ContactComponent3
