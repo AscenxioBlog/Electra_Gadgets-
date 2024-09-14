bf67528f13ddb7bef9c30fff86e4ae38a365095a
chidera-collins <chideracollins24@gmail.comm>
2024-09-14 20:31:36 +0100

    Service Third Component

--- client/src/UI/ServiceComponent/ServiceComponent.jsx
+++ client/src/UI/ServiceComponent/ServiceComponent.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ServiceComponent1 from './ServiceComponent1'
 import ServiceComponent2 from './ServiceComponent2'
+import ServiceThirdComponent from './ServiceThirdComponent'
 
 function ServiceComponent() {
   return (
     <div className=''>
       <ServiceComponent1/>
       <ServiceComponent2/>
+      <ServiceThirdComponent/>
       
     </div>
   )
