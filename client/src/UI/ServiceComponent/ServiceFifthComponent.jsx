0aee1c191372a0027728c8b42029f637c1ff181c
chidera-collins <chideracollins24@gmail.comm>
2024-09-14 23:03:46 +0100

    Service Final Part By Collins

---
+++ client/src/UI/ServiceComponent/ServiceFifthComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ServiceFifthComponent() {
+  return (
+    <div className='h-[299px] bg-red-950'>
+
+    </div>
+  )
+}
+
+export default ServiceFifthComponent
---
+++ client/src/UI/ServiceComponent/ServiceSixthComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AboutSixthComponent() {
+  return (
+    <div className='h-[150px] flex flex-col items-center justify-center mt-[70px] text-center'>
+      <h1>OUR AWARD</h1>
+      <h1 className='font-bold text-[30px]'>BEST CUSTOMERS SUPPORT</h1>
+      <h1 className='font-bold text-2xl'>_ _____ _</h1>
+    </div>
+  )
+}
+
+export default AboutSixthComponent
