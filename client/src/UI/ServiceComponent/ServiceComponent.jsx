c77cd3da07aca3aa027070dd8b54377107694716
chidera-collins <chideracollins24@gmail.comm>
2024-09-14 21:39:31 +0100

    Service Fourth Component Edit By Collins

--- client/src/UI/ServiceComponent/ServiceComponent.jsx
+++ client/src/UI/ServiceComponent/ServiceComponent.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ServiceComponent1 from './ServiceComponent1'
 import ServiceComponent2 from './ServiceComponent2'
 import ServiceThirdComponent from './ServiceThirdComponent'
+import ServiceFourthComponent from './ServiceFourthComponent'
 
 function ServiceComponent() {
   return (
@@ -9,6 +10,7 @@ function ServiceComponent() {
       <ServiceComponent1/>
       <ServiceComponent2/>
       <ServiceThirdComponent/>
+      <ServiceFourthComponent/>
       
     </div>
   )
