98e494ac9be6ee170f041edb3f4924b23b392a06
Ascenxio Blog <hassanyusuuf122@gmail.com>
2024-09-15 11:54:26 +0100

    footer and header updated by Ascenxio

--- client/src/ConstantComponent/FooterComponent/FooterComponent.jsx
+++ client/src/ConstantComponent/FooterComponent/FooterComponent.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaFacebook } from "react-icons/fa";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGooglePlus } from "react-icons/fa";
 
 function FooterComponent() {
   return (
     <div>
-      
+       <footer>
+
+<div className="foot min-h-[200px] w-full bg-MainBg grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-5">
+    <div className=" flex flex-col place-items-center space-y-4 pt-[50px]">
+        <h1 className=' text-[25px] font-bold'>Company</h1>
+        <ol className=' text-[17px] space-y-4'>
+            <li><Link to="/About">About</Link></li>
+            <li><Link>Careers</Link></li>
+            <li><Link>Terms</Link></li>
+            <li><Link>Privacy Policy</Link></li>
+        </ol>
+    </div>
+
+
+    <div className=" flex flex-col place-items-center space-y-4 pt-[50px]">
+    <h1 className=' text-[25px] font-bold'>Quick Links</h1>
+        <ol className=' text-[17px] space-y-4'>
+            <li><Link>Create Account</Link></li>
+            <li><Link>Login</Link></li>
+            <li><Link to="/product">Product</Link></li>
+            <li><Link to="/service">Service</Link></li>
+            <li><Link>Admin</Link></li>
+        </ol>
+    </div>
+
+
+    <div className="  flex flex-col place-items-center space-y-4 pt-[50px]">
+    <h1 className=' text-[25px] font-bold'>Supports</h1>
+        <ol className=' text-[17px] space-y-4'>
+            <li><Link>Contact Support</Link></li>
+            <li><Link>Frequently Asked Questiond (FAQ)</Link></li>
+        </ol>
+    </div>
+
+
+    <div className="  flex flex-col place-items-center space-y-4 pt-[50px]">
+    <h1 className=' text-[25px] font-bold'>Contact Us</h1>
+    <div className=" w-full flex gap-2 justify-center">
+           <section className=' h-[40px] w-[40px] rounded-[50%] bg-slate-600 text-[28px] text-slate-400 flex justify-center items-center hover:bg-MainBg transition-all hover:scale-[1.2]'><FaFacebook /></section>
+            <section className=' h-[40px] w-[40px] rounded-[50%] bg-slate-600 text-[28px] text-slate-400 flex justify-center items-center hover:bg-MainBg transition-all hover:scale-[1.2]'><AiFillTwitterCircle /></section>
+            <section className=' h-[40px] w-[40px] rounded-[50%] bg-slate-600 text-[28px] text-slate-400 flex justify-center items-center hover:bg-MainBg transition-all hover:scale-[1.2]'><FaLinkedin /></section>
+            <section className=' h-[40px] w-[40px] rounded-[50%] bg-slate-600 text-[28px] text-slate-400 flex justify-center items-center hover:bg-MainBg  transition-all hover:scale-[1.2]'><FaGooglePlus /></section>
+           </div>
+    </div>
+</div>
+</footer>
     </div>
   )
 }
--- client/src/ConstantComponent/HeaderComponent/HeaderComponent.jsx
+++ client/src/ConstantComponent/HeaderComponent/HeaderComponent.jsx
@@ -2,12 +2,41 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { FaCartPlus } from "react-icons/fa6";
 import { FaUserPlus } from "react-icons/fa6";
+import { IoMdMenu } from "react-icons/io";
 
 function HeaderComponent() {
   return (
     <div>
-        <div className='h-[70px] bg-[white] z-30 grid grid-cols-2 fixed top-0 w-[100%] md:grid-cols-2 lg:h-[70px] lg:grid-cols-4 lg:flex lg:items-center justify-between'>
-               <div className='text-[30px] gap-2 p-[20px] text-[#FD282A] flex items-center md:text-[40px] md:ml-[30px] '>
+        <div className='h-[70px] bg-white z-30 grid grid-cols-[60%,40%] md:grid-cols-[40%,60%] fixed top-0 w-[100%]  lg:h-[70px] lg:grid-cols-[40%,20%,40%] ' style={{boxShadow:'0 0 25px black'}}>
+         <div className=" hidden lg:inline-block lg:mt-5">
+         <div className=" bg-[] flex justify-end items-center ">
+            <ol  className=' flex  gap-10 text-[20px] font-semibold mr-6'>
+              <li><Link  to='/'>Home</Link></li>
+              <li><Link to='/about'>About</Link></li>
+              <li><Link to='/product'>Product</Link></li>
+            </ol>
+          </div>
+         </div>
+          <div className=" bg-[] flex justify-center items-center">
+            <div className=" head h-[60px] w-[100%] bg-[] text-[30px] font-bold flex justify-center items-center" style={{boxShadow:'0 0 15px #FABC3F'}}>
+              <h1 className=''>Electra_Gadget</h1>
+            </div>
+          </div>
+          <div className=" bg-[] flex justify-end  lg:justify-between items-center">
+           <div className=" hidden lg:inline-block">
+           <ol className=' flex  gap-10 text-[20px] font-semibold ml-6'>
+              <li><Link to='/service'>Service</Link></li>
+              <li><Link to='/contact'>Contact</Link></li>
+              <li><Link to=''>Login</Link></li>
+            </ol>
+           </div>
+            <section className=" flex text-[20px] gap-3 pr-5 ">
+              <span><FaUserPlus /></span>
+              <span><FaCartPlus /></span>
+              <button className=' lg:hidden'><IoMdMenu /></button>
+            </section>
+          </div>
+               {/* <div className='text-[30px] gap-2 p-[20px] text-[#FD282A] flex items-center md:text-[40px] md:ml-[30px] '>
                     <i class="fa-solid fa-cart-shopping"></i>
                     <span className='text-[black] text-[30px] font-bold md:text-[40px] lg:text-3xl'>Electra_Gadget</span>
                </div>
@@ -25,10 +54,10 @@ function HeaderComponent() {
                <div className='text-[40px] mt-5 ml-[140px] inline-block md:text-[40px] md:ml-80 lg:hidden'><i class="fa-solid fa-bars"></i></div>
 
                <div className='hidden gap-[40px] lg:flex lg:items-center lg:justify-center lg:pr-[70px]'>
-                   <div className='bg-[#F1F1F1] h-[50px] w-[50px] rounded-[50%] flex justify-center items-center hover:bg-[#fd282ca4]'><FaUserPlus /></div>
+                   <div className='bg-[#F1F1F1] h-[50px] w-[50px] rounded-[50%] flex justify-center items-center hover:bg-[#fd282ca4]'></div>
                    <div className='bg-[#F1F1F1] rounded-[50%] h-[50px] w-[50px] flex justify-center items-center'><i class="fa-regular fa-heart"></i></div>
-                   <div className='bg-[#F1F1F1] rounded-[50%] h-[50px] w-[50px] flex justify-center items-center'><FaCartPlus /></div>
-               </div>
+                   <div className='bg-[#F1F1F1] rounded-[50%] h-[50px] w-[50px] flex justify-center items-center'></div>
+               </div> */}
            </div>
     </div>
   )
